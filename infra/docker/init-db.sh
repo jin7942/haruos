1967931936e78a47991153d6e702908ca5db7087
jin7942 <infomer.jin@gmail.com>
2026-03-24 15:01:22 +0000

    fix: docker compose 민감정보 환경변수 분리 (.env 주입 방식)

--- infra/docker/init-db.sh
+++ infra/docker/init-db.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # PostgreSQL 초기화: console DB + tenant DB 분리 생성
 # docker-entrypoint-initdb.d/ 에서 자동 실행됨
+# 환경변수: CONSOLE_DB_NAME, TENANT_DB_NAME (docker-compose에서 주입)
 set -e
 
+CONSOLE_DB="${CONSOLE_DB_NAME:-haruos_console}"
+TENANT_DB="${TENANT_DB_NAME:-haruos_tenant}"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE haruos_console;
-    CREATE DATABASE haruos_tenant;
+    CREATE DATABASE ${CONSOLE_DB};
+    CREATE DATABASE ${TENANT_DB};
 EOSQL
 
 # pgvector 확장 활성화
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d haruos_console <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "${CONSOLE_DB}" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d haruos_tenant <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "${TENANT_DB}" <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS vector;
 EOSQL
